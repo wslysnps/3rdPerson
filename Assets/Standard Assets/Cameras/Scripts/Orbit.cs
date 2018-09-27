112d9fc598e0b04bfd1b260c440b7aec7acc1898
wslysnps <patrickworm@gmail.com>
2018-09-27 21:50:00 +0200

    Initial commit

---
+++ Assets/Standard Assets/Cameras/Scripts/Orbit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour
+{
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(player.position.x + 0.0f, player.position.y + 2.0f, player.position.z + 1.0f);
+    }
+
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
